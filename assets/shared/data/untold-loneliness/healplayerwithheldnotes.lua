47909ded8e710f91ba0905414644631c5d018747
NamsJr6 <rubensdms2024@gmail.com>
2026-04-01 10:06:50 +0100

    untold
    uhh this is untold lonliness working in progress

--- assets/shared/data/untold-loneliness/healplayerwithheldnotes.lua
+++
@@ -1,8 +0,0 @@
-local damageAmount = 0.01
-local sustainHeal = 0.008 
-
-function goodNoteHit(id, direction, noteType, isSustainNote)
-    if isSustainNote then
-        setProperty('health', getProperty('health') + sustainHeal)
-    end
-end
