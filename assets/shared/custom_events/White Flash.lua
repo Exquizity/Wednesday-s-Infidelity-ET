1b1cb7d62ca259028b69d895f9b70097d17e5423
Exquizity <wiktor.roblox2012@gmail.com>
2026-04-02 13:16:11 +0100

    White Flash / Black Flash
    JJK REFERENCE HAAAAHAHAHAHAHA!

--- assets/shared/custom_events/White Flash.lua
+++ assets/shared/custom_events/White Flash.lua
@@ -1,21 +1,27 @@
 function onCreate()
-  makeLuaSprite('whiteflash', '', -200, -200)
-  makeGraphic('whiteflash', 1920, 1080, 'FFFFFF')
-  setScrollFactor('whiteflash', 0, 0)
-  setObjectCamera('whiteflash', 'other')
-  addLuaSprite('whiteflash', true)
-  setProperty('whiteflash.alpha', 0)
+    makeLuaSprite('whiteflash', '', -200, -200)
+    makeGraphic('whiteflash', 1920, 1080, 'FFFFFF')
+    setScrollFactor('whiteflash', 0, 0)
+    setObjectCamera('whiteflash', 'other')
+    addLuaSprite('whiteflash', true)
+    setProperty('whiteflash.alpha', 0)
+
+    makeLuaSprite('blackflash', '', -200, -200) --jjk reference trust
+    makeGraphic('blackflash', 1920, 1080, 'FFFFFF')
+    setScrollFactor('blackflash', 0, 0)
+    setObjectCamera('blackflash', 'other')
+    addLuaSprite('blackflash', true)
+    setProperty('blackflash.alpha', 0)
 end
+
 function onEvent(name, value1, value2)
   if name == 'White Flash' then
-    setProperty('whiteflash.alpha', 1)
-    doTweenAlpha('whiteflashbye', 'whiteflash', 0, 1, 'linear')
-  end
-end
-function onUpdate()
-  haha = false
-  if getPropertyFromClass('flixel.FlxG', 'keys.justPressed.T') and not haha == true then
-    setProperty('whiteflash.alpha', 1)
-    doTweenAlpha('whiteflashbye', 'whiteflash', 0, 1, 'linear')
-  end
+      if string.lower(value1) ~= 'b' then
+          setProperty('whiteflash.alpha', 1)
+          doTweenAlpha('whiteflashbye', 'whiteflash', 0, 1, 'linear')
+      else
+          setProperty('whiteflash.alpha', 1)
+          doTweenAlpha('whiteflashbye', 'whiteflash', 0, 1, 'linear')
+      end
+   end
 end
