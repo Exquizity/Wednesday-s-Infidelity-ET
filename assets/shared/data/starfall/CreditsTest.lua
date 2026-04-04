5caa91d2cc7b0aa30c6a7c01d586812aba786ea2
NamsJr6 <rubensdms2024@gmail.com>
2026-04-04 21:08:28 +0100

    Starfall Credits

---
+++ assets/shared/data/starfall/CreditsTest.lua
@@ -0,0 +1,48 @@
+local font = 'vcr.ttf'
+local colour = '9B78C2'
+local activationstep = 128
+
+local songName = 'Starfall'
+local info = --Song Author, Sprite Maker, Charters, Extra Info
+    '\nMusic Composed By: Doge'..
+    '\nSprites By: laik7u7'..
+    '\nCharted By: NamsJr6'..
+    '\nBackground By: Wednesdays Infidelity and NamsJr6'
+
+function onCreatePost()
+    makeLuaText('songname', songName, screenWidth, 0, screenHeight/2-100)
+    setTextSize('songname', '65')
+    setTextFont('songname', font)
+    setTextAlignment('songname', 'center')
+    setTextColor('songname', color)
+    setTextBorder('songname', '000000')
+	setObjectCamera('songname', 'other')
+	setProperty('songname.antialiasing', false)
+	setProperty('songname.alpha', 0)
+    addLuaText('songname', true)
+    makeLuaText('desc', info, screenWidth, 0, screenHeight/2-20)
+	setTextSize('desc', 35)
+	setTextFont('desc', font)
+	setTextAlignment('desc', 'center')
+	setTextColor('desc', colour)
+	setTextBorder('desc', 0, '000000')
+	setObjectCamera('desc', 'other')
+	setProperty('desc.antialiasing', false)
+	setProperty('desc.alpha', 0)
+    addLuaText('desc', true)
+end
+
+function onStepHit()
+    if curStep == activationstep then
+        doTweenAlpha('songtween', 'songname', 1, 1, 'linear')
+        doTweenAlpha('desctween', 'desc', 1, 1, 'linear')
+        runTimer('finish', 5.5, 1)
+    end
+end
+
+function onTimerCompleted(tag)
+    if tag == 'finish' then
+        doTweenAlpha('songtween', 'songname', 0, 1, 'linear')
+        doTweenAlpha('desctween', 'desc', 0, 1, 'linear')
+    end
+end
