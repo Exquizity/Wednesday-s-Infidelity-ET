0e4de1ca9164c34960e0424ebbdfe753e1e8af7c
NamsJr6 <rubensdms2024@gmail.com>
2026-04-04 19:54:02 +0100

    New level
    Starfall

--- assets/shared/stages/OswaldMap.lua
+++ assets/shared/stages/OswaldMap.lua
@@ -1,7 +1,12 @@
 function onCreate()
-    makeLuaSprite('fenceBg','backgrounds/BG_OSWALD', -600, -300)
+    makeLuaSprite('fenceBg','backgrounds/BG_OSWALD', -700, -300)
+    scaleLuaSprite('fenceBg', 1.02, 1.02)
     addLuaSprite('fenceBg',false)
-    makeLuaSprite('dark','backgrounds/darkos', -600, -300)
+    makeLuaSprite('dark','backgrounds/darkos', -700, -300)
     addLuaSprite('dark',false)
     setProperty('dark.alpha', 0)
+
+    makeLuaSprite('phase3','backgrounds/OSWALD-STARFALL-PHASES/PHASE3', -700, -300)
+    addLuaSprite('phase3',false)
+    setProperty('phase3.alpha', 0)
 end
